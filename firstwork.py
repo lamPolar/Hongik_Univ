a2bd5b1dbe97621fe768248104e3a9a1b79c4196
lamPolar <46983641+lamPolar@users.noreply.github.com>
2021-05-14 13:57:54 +0900

    Update firstwork.py
    added print results

--- firstwork.py
+++ firstwork.py
@@ -4,6 +4,10 @@ area = radius * radius * 3.14159
 volume = area * length
 print("The area is " + str(area))
 print("The volume is " + str(volume), end = '\n\n')
+#Enter the radius and length of a cylinder: 5,10
+#The area is 78.53975
+#The volume is 785.3975
+
 
 #question 2
 x=10
@@ -12,10 +16,10 @@ Q2_1 = ((1 -( 1/x**5)))**-1
 Q2_2 = (3*y)/((4*y)-8)
 print("Q2_1 is " + str(Q2_1))
 print("Q2_2 is " + str(Q2_2), end = '\n\n')
-
+#Q2_1 is 1.000010000100001
+#Q2_2 is 1.25
 
 #question 3
-
 print("\uac10 \uc0ac", end = '\n\n')
 print("\ud76c \ub9dd")
-
+#감 사\n\n희 망
