9790b4daac169d95234752012775cdc79ad60233
lamPolar <46983641+lamPolar@users.noreply.github.com>
2021-05-14 13:44:16 +0900

    2016.03.17
    lab1

---
+++ firstwork.py
@@ -0,0 +1,21 @@
+# question 1
+radius, length = eval(input("Enter the radius and length of a cylinder: "))
+area = radius * radius * 3.14159
+volume = area * length
+print("The area is " + str(area))
+print("The volume is " + str(volume), end = '\n\n')
+
+#question 2
+x=10
+y=5
+Q2_1 = ((1 -( 1/x**5)))**-1
+Q2_2 = (3*y)/((4*y)-8)
+print("Q2_1 is " + str(Q2_1))
+print("Q2_2 is " + str(Q2_2), end = '\n\n')
+
+
+#question 3
+
+print("\uac10 \uc0ac", end = '\n\n')
+print("\ud76c \ub9dd")
+
---
+++ test.py
@@ -0,0 +1,4 @@
+print ("Hello world")
+a = 10
+b = a**2
+print (str(b))
