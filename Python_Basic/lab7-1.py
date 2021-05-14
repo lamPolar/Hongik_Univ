2e2daca30a40e87c681dd766c9f64be6918b74bc
lamPolar <46983641+lamPolar@users.noreply.github.com>
2021-05-14 16:29:42 +0900

    lab 7 설명 추가 lab7-1-1 solving again

--- Python_Basic/lab7-1.py
+++ Python_Basic/lab7-1.py
@@ -1,21 +1,21 @@
 #lab7-1 Hey Jude
-#1.
+
 song = "Hey Jude, don't make it bad Take a sad song and make it better Remember to let her into your heart Then you can start to make it better"
 
+#1. song을 단어 단위로 resverse해서 print
 list1 = song.split()
-
 list1.reverse()
+#solving again
+print(" ".join(list1))
 
-num = len(list1)
+"""num = len(list1)
 for i in range(0,num):
-	print(list1[i], end = ' ')
+	print(list1[i], end = ' ')"""
 
-
-#2.
+#2. song에 있는 알파벳 세기
 song.lower()
-list3 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+list2 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 print('\n')
-for alpha in list3:
+for alpha in list2:
 	alphanum = song.count(alpha)
 	print(alpha, 'count is', alphanum)
-
--- Python_Basic/lab7-2.py
+++ Python_Basic/lab7-2.py
@@ -1,5 +1,8 @@
 #lab7-2 time class
-
+#clock class 생성
+#멤버 변수로 hour, minute가 있고
+# addminute분을 더한 시간을 알려주는 add와 , minus0minute분을 뺀 시간을 알려주는 sub
+#현재 object의 멤버변수를 return하는 str 함수가 있다.
 class clock:
 	def __init__(self,hour,minute):
 		self.hour = hour
@@ -19,19 +22,19 @@ class clock:
 
 	def __str__(self):
 		return str(self.hour) + "hours and " + str(self.minute) + "minutes."
+
 def main():
 	clock1 = clock(10, 20)
 	print(clock1)
-	
+
 	addminute = 90
 	print("\nAdd " + str(addminute) + 'minute')
 	clock2 = clock1.__add__(addminute)
 	print(clock2)
-	
+
 	minusminute = 60
 	print('\nMinus ' + str(minusminute) + 'minute')
 	clock3 = clock1.__sub__(minusminute)
 	print(clock3)
 
 main()
-
