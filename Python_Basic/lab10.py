53739162658e16231686825980d8c5b13c0ca6a2
lamPolar <46983641+lamPolar@users.noreply.github.com>
2021-05-14 18:46:11 +0900

    lab10 수정 + solving again

--- Python_Basic/lab10.py
+++ Python_Basic/lab10.py
@@ -1,26 +1,27 @@
-#lab10.py
-
+#lab10.py - solving again
+#lab10_dic.txt를 열어서 한줄씩 보며
+#S, F, D 순서대로 list를 만들고
+#영어와 대치되도록 dictionary에 저장
 def main():
-	file = open("c:\lab10\dic.txt", "r")
-	for line in file:
-		string = file.readline()
-		words = string.split()
+	file = open("lab10_dic.txt", "r")
+	dict1 = dict()
+	while True:
+		line = file.readline()
+		if not line:
+			break
+		words = line.split()
 		list1 = []
-		dict = {}
+		if words == []: #newline의 경우
+			continue
 		for x in words:
-			if S in words[x]:
-				words[x].insert(list1)
-			elif F in words[x]:
-				words[x].insert(list1[1])
-			elif D in words[x]:
-				words[x].inser(list1[2])
-		dict[word[0]:list1]
-	for key in dict:
-		print(key + ":" + str(dict[key]))
-
+			if "S" in x:
+				list1.insert(0,x)
+			elif "F" in x:
+				list1.insert(1,x)
+			elif "D" in x:
+				list1.insert(2,x)
+		dict1[words[0]]=list1
+	for key in dict1:
+		print(key + ":" + str(dict1[key]))
 	file.close()
 main()
-1
-
-
-	
