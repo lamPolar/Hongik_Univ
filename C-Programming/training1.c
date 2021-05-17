b0a2cbccdc2274bd7b207c9b152eca4eec26f07e
lamPolar <46983641+lamPolar@users.noreply.github.com>
2021-05-17 22:11:58 +0900

    training1.c solving again

--- C-Programming/training1.c
+++ C-Programming/training1.c
@@ -2,8 +2,17 @@
 #include<stdio.h>
 
 int main(void){
-  int n1,n2,n3,n4,n5,n6;
-  printf("정수 6개를 입력하세요: \n");
-  for i in range(0,6):
-    printf(i)
+  int n;
+  int *list;
+  float avg1,avg2;
+  printf("정수 6개를 입력하세요: ");
+  for(int i=0;i<6;i++){
+    scanf("%d",&n);
+    list[i] = n;}
+  //홀수번째 입력값
+  avg1 = (list[0]+list[2]+list[4])/3;
+  //짝수번째 입력값
+  avg2 = (list[1]+list[3]+list[5])/3;
+  printf("홀수번째 입력값들의 평균 : %f \n\
+짝수번째 입력값들의 평균 : %f \n",avg1,avg2);
 }
